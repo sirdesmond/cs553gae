59d3fdd418de47daa3a7e9ec7856342d28e89a12
Kevin Brandstatter <kjbrandstatter@gmail.com>
2014-11-05 00:08:45 -0600

    file content now 100 char lines

--- make-files.sh
+++ make-files.sh
@@ -4,52 +4,40 @@ DIR=$1
 
 [[ ! -d $DIR ]] && mkdir $DIR
 
-function random_name {
-   FNAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+function random_string {
+   COUNT=$1
+   FNAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $COUNT | head -n 1)
    while [ -e $DIR/$FNAME ]; do
-      FNAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+      FNAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $COUNT | head -n 1)
    done
    echo $FNAME
 }
 
-function gen_sequential {
-
-   for i in 1 10 100 1024; do
-      SIZE=$(( i ))
-      for j in $(seq 1 100); do
-         dd if=/dev/urandom of=$DIR/file-$SIZE-${j}.raw bs=1024 count=$SIZE
-      done
-   done
-
-   SIZE=$(( 10 * (2 ** 10) ))
-   for j in $(seq 1 10); do
-      dd if=/dev/urandom of=$DIR/file-$SIZE-${j}.raw bs=1024 count=$SIZE
-
-   done
-
-   SIZE=$(( SIZE * 10 ))
-   dd if=/dev/urandom of=$DIR/file-$SIZE-1.raw bs=1024 count=$SIZE
-}
-
 function gen_random_names {
    for i in 1 10 100 1024; do
-      SIZE=$(( i ))
+      SIZE=$(( i * 1024 ))
       for j in $(seq 1 100); do
-         NAME=$(random_name)
-         dd if=/dev/urandom of=$DIR/$NAME bs=1024 count=$SIZE
+         NAME=$(random_string 10)
+         for k in $(seq 1 100 $SIZE ); do
+            random_string 100 >> $DIR/$NAME
+         done
       done
    done
 
-   SIZE=$(( 10 * (2 ** 10) ))
+   SIZE=$(( 10 * (2 ** 20) ))
    for j in $(seq 1 10); do
-      NAME=$(random_name)
-      dd if=/dev/urandom of=$DIR/$NAME bs=1024 count=$SIZE
+      NAME=$(random_string 10)
+      for k in $(seq 1 100 $SIZE ); do
+         random_string 100 >> $DIR/$NAME
+      done
 
    done
 
    SIZE=$(( SIZE * 10 ))
-   NAME=$(random_name)
-   dd if=/dev/urandom of=$DIR/$NAME bs=1024 count=$SIZE
+   NAME=$(random_string 10)
+   for k in $(seq 1 100 $SIZE ); do
+      random_string 100 >> $DIR/$NAME
+   done
 }
 
 gen_random_names
