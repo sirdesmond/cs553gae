a69348601b8250f56437e1810a410205ed64d960
Kevin Brandstatter <kjbrandstatter@gmail.com>
2014-11-04 22:34:42 -0600

    make files now makes with random file names

--- make-files.sh
+++ make-files.sh
@@ -4,19 +4,52 @@ DIR=$1
 
 [[ ! -d $DIR ]] && mkdir $DIR
 
-for i in 1 10 100 1024; do
-   SIZE=$(( i ))
-   for j in $(seq 1 100); do
-      dd if=/dev/urandom of=$DIR/file-$SIZE-${j}.raw bs=1024 count=$SIZE
+function random_name {
+   FNAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+   while [ -e $DIR/$FNAME ]; do
+      FNAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
    done
-done
+   echo $FNAME
+}
 
-SIZE=$(( 100 * (2 ** 10) ))
-for j in $(seq 1 10); do
-   dd if=/dev/urandom of=$DIR/file-$SIZE-${j}.raw bs=1024 count=$SIZE
+function gen_sequential {
 
-done
+   for i in 1 10 100 1024; do
+      SIZE=$(( i ))
+      for j in $(seq 1 100); do
+         dd if=/dev/urandom of=$DIR/file-$SIZE-${j}.raw bs=1024 count=$SIZE
+      done
+   done
 
-SIZE=$(( SIZE * 10 ))
-dd if=/dev/urandom of=$DIR/file-$SIZE-1.raw bs=1024 count=$SIZE
+   SIZE=$(( 10 * (2 ** 10) ))
+   for j in $(seq 1 10); do
+      dd if=/dev/urandom of=$DIR/file-$SIZE-${j}.raw bs=1024 count=$SIZE
 
+   done
+
+   SIZE=$(( SIZE * 10 ))
+   dd if=/dev/urandom of=$DIR/file-$SIZE-1.raw bs=1024 count=$SIZE
+}
+
+function gen_random_names {
+   for i in 1 10 100 1024; do
+      SIZE=$(( i ))
+      for j in $(seq 1 100); do
+         NAME=$(random_name)
+         dd if=/dev/urandom of=$DIR/$NAME bs=1024 count=$SIZE
+      done
+   done
+
+   SIZE=$(( 10 * (2 ** 10) ))
+   for j in $(seq 1 10); do
+      NAME=$(random_name)
+      dd if=/dev/urandom of=$DIR/$NAME bs=1024 count=$SIZE
+
+   done
+
+   SIZE=$(( SIZE * 10 ))
+   NAME=$(random_name)
+   dd if=/dev/urandom of=$DIR/$NAME bs=1024 count=$SIZE
+}
+
+gen_random_names
